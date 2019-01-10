2395da6c7f2366fb2181303a2fec48681c9b69b7
Beniamin Bia <biabeniamin@gmail.com>
2019-01-10 13:49:03 +0200

    adder class defined

---
+++ Adder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaderFooterAdded
+{
+    public static class Adder
+    {
+        public static void AddHeaderAndFooter(Image image)
+        {
+
+        }
+
+    }
+}
